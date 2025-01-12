0a61e789bfcac00f4c0596834dd74e586e715011
Cao Zhong Z <zhong.z.cao@intel.com>
2025-01-12 19:42:31 +0800

    Futher optimize performance and expand coverage
    Signed-off-by: Cao Zhong Z <zhong.z.cao@intel.com>

--- concept.cpp
+++ concept.cpp
@@ -1,5 +1,109 @@
 #include <dnnl_ext.hpp>
 
+enum joint_dtypes {
+  _fp32 = 0,
+  _f16, _bf16, int8, _f16_int4, _bf16_int4
+};
+
+enum transposes {
+  _nn = 0, _nt, _tn, _tt
+};
+
+template <enum transposes tr, enum joint_dtypes>
+struct onednn_tag_mapper;
+
+template <enum joint_dtypes Ts>
+struct onednn_tag_mapper<_nt> {
+  static inline std::tuple<dnnl::tag, dnnl::tag> do() {
+    return make_tuple(memory::format_tag::ab, memory::format_tag::ba);
+  };
+};
+
+template <enum joint_dtypes Ts>
+struct onednn_tag_mapper<_nn> {
+  static_assert(Ts != _f16_int4 || Ts != _bf16_int4, "Reason why");
+  static inline std::tuple<dnnl::tag, dnnl::tag> do() {
+    return make_tuple(memory::format_tag::ab, memory::format_tag::ba);
+  };
+};
+
+template <enum joint_dtypes Ts>
+struct onednn_types_mapper;
+
+template <> struct onednn_types_mapper<_f16_int4> {
+  static inline std::tuple<
+    memory::data_type,
+    memory::data_type,
+    memory::data_type,
+    memory::data_type> do() {
+      return make_tuple(memory::data_type::f16,
+          memory::data_type::u4, memory::data_type::f16, memory::data_type::f16);
+    }
+};
+
+template <> struct onednn_types_mapper<_bf16_int4> {
+  static inline std::tuple<
+    memory::data_type,
+    memory::data_type,
+    memory::data_type,
+    memory::data_type> do() {
+      return make_tuple(memory::data_type::bf16,
+          memory::data_type::u4, memory::data_type::bf16, memory::data_type::bf16);
+    }
+};
+
+template <transposes tr, enum joint_dtypes, bool bias, F f_attr>
+struct create_matmul {
+  static primitive_ext run(int device_id) {
+    // write everything inside this function with if constexpr clauses
+  }
+private:
+  // if default constructor of primitive cache could read the environment variable
+  // then it'll save a lot of trouble
+  static thread_local std::array<primitive_cache, 16> mappings {};
+
+  // this won't be needed if primitive_cache have good default constructor
+  static primitive_cache& get_cache(int device_id) {
+    auto mapping = mappings[device_id];
+    if (!mapping.initialized()) {
+      // construct the cache
+    } else
+      return mappings[device_id];
+  }
+}
+
+template <typename F>
+primitive_ext create_matmul(joint_data_types types, transposes tr, int device_id, F f_attr) {
+  switch(types) {
+  case _f16_int4:
+    return create_matmul<_f16_int4, F>(tr, device_id, f_attr);
+  case _bf16_int4:
+    return create_matmul<_bf16_int4, F>(tr, device_id, f_attr);
+  default:
+    throw std::exception();
+  }
+}
+
+template <enum joint_data_types types, int device_id, typename F>
+primitve_ext create_matmul(transposes tr, F f_attr) {
+  switch(tr) {
+  case _nn:
+    return create_matmul<types, device_id, _nn, F>(f_attr);
+  case _nt:
+    return create_matmul<types, device_id, _nt, F>(f_attr);
+  case _tn:
+    return create_matmul<types, device_id, _tn, F>(f_attr);
+  case _tt:
+    return create_matmul<types, device_id, _tt, F>(f_attr);
+  default:
+    throw std::exception();
+  }
+}
+
+template <enum joint_data_types types, int device_id, transposes tr, typename F>
+primitve_ext create_matmul(F f_attr) {
+}
+
 template <typename F>
 primitive_ext create_matmul_int4(const tensor... tensors, F f_attr) {
   auto key = shapes_of_all_tensors(tensors);
